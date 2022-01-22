9a56d1247ed97d543d62185e7759e161725b2492
james-muriithi <muriithijames556@gmail.com>
2022-01-22 11:37:50 +0300

    added user functions

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from user import User
 
 user1 = User("James", "Muriithi", "james-muriithi", "password")
+user2 = User("James2", "Muriithi2", "james-muriithi2", "password")
 
-print(user1)
+user1.save_user()
+user2.save_user();
+
+print(User.user_accounts)
--- user.py
+++ user.py
@@ -1,11 +1,24 @@
 class User:
-    user_accounts = []
 
+
+    # user constructor
     def __init__(self, first_name, last_name, username, password):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
         self.password = password
+
+    user_accounts = []
+
+    # function to delete user account
+    def delete_user(self):
+        User.user_accounts.remove(self)
+
+    @classmethod
+    def find_user(cls, username):
+        for user in cls.user_accounts:
+            if user.username == username:
+                return user
 
     def save_user(self):
         # Append user to the user accounts lists
@@ -16,5 +29,5 @@ class User:
         return cls.user_accounts
 
     def __repr__(self):
-        return f'Fullname: {self.first_name} {self.last_name}\nUsername: {self.username}'
+        return f'Fullname: {self.first_name} {self.last_name} Username: {self.username}'
 
