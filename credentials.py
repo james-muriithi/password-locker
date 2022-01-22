a4b852a82f6b6e5f535cb6b24b503fe6145d9bef
james-muriithi <muriithijames556@gmail.com>
2022-01-22 11:42:25 +0300

    added credentials constructor

---
+++ credentials.py
@@ -0,0 +1,8 @@
+class Credetials:
+    user_password = []
+
+    def __init__(self, platform, username, password):
+        self.platform = platform
+        self.username = username
+        self.password = password
+
