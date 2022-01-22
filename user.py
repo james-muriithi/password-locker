2e305bd673fe9991a3f2ed794c765bae6bd2ce86
james-muriithi <muriithijames556@gmail.com>
2022-01-22 11:01:35 +0300

    added user constructor

--- user.py
+++ user.py
@@ -1,1 +1,10 @@
 class User:
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'Fullname: {self.first_name} {self.last_name}\nUsername: {self.username}'
+
