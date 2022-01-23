8f28a21cfe84f8e65e8e0c5124510baa6a073fdc
james-muriithi <muriithijames556@gmail.com>
2022-01-23 19:16:34 +0300

    added user tests

--- user_test.py
+++ user_test.py
@@ -5,6 +5,7 @@ from user import User
 class TestUser(unittest.TestCase):
     def setUp(self):
         self.user = User("james", "muriithi", "james-muriithi", "password")
+        User.user_accounts = []
 
 
     def test_user_created(self):
@@ -14,7 +15,24 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.user.password, "password")
 
     def test_user_saved(self):
-        self.assertEqual(len(User.display_user_accounts()), 0)
         self.user.save_user()
         self.assertEqual(len(User.display_user_accounts()), 1)
+
+    def test_user_delete(self):
+        self.user.save_user()
+        self.assertEqual(len(User.display_user_accounts()), 1)
+        self.user.delete_user()
+        self.assertEqual(len(User.display_user_accounts()), 0)
+
+    def test_login(self):
+        self.user.save_user()
+        self.assertEqual(User.login(self.user.username, self.user.password), True)
+
+    def test_find_user(self):
+        self.user.save_user()
+        self.assertEqual(User.find_user(self.user.username), self.user)
+
+    def test_display_user_accounts(self):
+        self.user.save_user()
+        self.assertEqual(User.display_user_accounts(), User.user_accounts)
 
